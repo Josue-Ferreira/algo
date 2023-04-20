94355bbd86c1e2c308f6fabc451ed2afeccd1b0e
Ferreira Josué <ferreira.josu@gmail.com>
2023-04-20 15:33:34 +0200

    sorted function

--- app.js
+++ app.js
@@ -13,4 +13,19 @@ function findSmallest (note){
     return smallest;
 }
 
-console.log(findSmallest(note));
+function sorteNote (note) {
+    let sortedNote = [], min, index_min, tab_length;
+    // Prend la longueur de note car note.length est remis à jour dans le for
+    tab_length = note.length;
+    // Parcours à chaque fois le tableau, prends le min et le supprime de note
+    for (let i=0; i < tab_length; i++) {
+        min = findSmallest(note);           // Cherche
+        sortedNote.push(min);               // Ajoute au tableau trié
+        index_min = note.indexOf(min);      // Donne l'index du nombre à supprimer
+        note.splice(index_min,1);           // Supprime le nombre à l'index donné
+    }
+    return sortedNote;                      // Renvoie le tableau trié
+}
+
+//console.log(findSmallest(note));
+console.log(sorteNote(note));
